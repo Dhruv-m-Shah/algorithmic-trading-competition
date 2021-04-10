5e9c058bd76c8d8c31923acb0a1c175d424f6eb5
Dhruv-m-Shah <shahdhruv2001@gmail.com>
2021-04-10 19:01:53 -0400

    set env variables, changed to redisCloud

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.use(
     resave: false,
     saveUninitialized: true,
     cookie: {
-      secure: false,
+      secure: true,
       httpOnly: true,
       maxAge: 1000 * 60 * 60, // 1 hour
     },
