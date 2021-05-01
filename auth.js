652a6f60d154a2b90ec8763319c757e2c4e39d51
Dhruv-m-Shah <shahdhruv2001@gmail.com>
2021-05-01 19:54:34 -0400

    added headers

--- auth.js
+++ auth.js
@@ -1,11 +1,13 @@
 const fetch = require('node-fetch');
 
 async function validateCaptcha(token) {
+  console.log(process.env.CAPTCHA_SECRET);
   const response = await fetch(
     `https://www.google.com/recaptcha/api/siteverify?
     secret=${process.env.CAPTCHA_SECRET}&response=${token}`,
     {
       method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" }
     }
   );
   console.log(await response.json());
