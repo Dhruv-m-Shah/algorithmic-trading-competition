a25f5d962988528a7aa284d2cbd49e09ef0a2803
Dhruv-m-Shah <shahdhruv2001@gmail.com>
2021-05-03 20:50:34 -0400

    Added EST timezone

--- cron.js
+++ cron.js
@@ -39,7 +39,7 @@ function updateStandardStock(client) {
 
 function executeLambdas(client) {
   cron.schedule(
-    "00 48 20 * * *",
+    "00 52 20 * * *",
     async () => {
       // Run cron job everyday at 4:30 EST.
       updateStandardStock(client);
